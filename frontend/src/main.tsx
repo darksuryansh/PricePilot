c1410cdbeda65383a4395eda3b5a54fa658ab046
darksuryansh <suryanshbhardwaj04@gmail.com>
2025-12-27 23:41:59 +0530

    fix: issue resolved

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,12 @@
   import App from "./App.tsx";
   import "./index.css";
   import { AuthProvider } from "./contexts/AuthContext";
-  import { Analytics } from "@vercel/analytics/next"
+  
 
   createRoot(document.getElementById("root")!).render(
     <AuthProvider>
       <App />
-      <Analytics/>
+   \
     </AuthProvider>
   );
   
